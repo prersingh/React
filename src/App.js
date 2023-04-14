8fecb1cd5fb9ceeddcbc72c912ea01406f752ce2
preranasinghh <preranasingh1176@gmail.com>
2023-04-14 16:32:03 +0530

    Task4

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import AddUser from './Components/Users/AddUser';
+
+
+function App() {
+  return (
+    <div>
+      <AddUser/>
+
+    </div>
+  );
+}
+
+export default App;
